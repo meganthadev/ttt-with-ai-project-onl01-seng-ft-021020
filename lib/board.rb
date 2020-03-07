e746461bce6e64575580330c072afa62bb0b633e
Megan Horsfield <meganthadev@gmail.com>
2020-03-07 21:13:11 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,28 @@
+class Board
+  
+  cells = []
+  
+  def reset! 
+    cells.clear 
+  end 
+  
+  def display
+  end 
+  
+  def position
+    get.strip(input).
+  end   
+  
+  def full?
+  end   
+  
+  def turn_count
+  end   
+  
+  def taken?
+  end 
+  
+  def valid_move?
+  end   
+  
+end   
---
+++ lib/players/computer.rb
@@ -0,0 +1,5 @@
+class Computer < Player 
+  
+  
+  
+end 
