c7aac2a6a8a76d06cea8f4c22990da238ca22f54
Megan Horsfield <meganthadev@gmail.com>
2020-03-08 01:49:02 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -38,7 +38,21 @@ class Game
   end
   
   def start 
-    
+    game = Game.new
+    until game.won?
+     game.play
+      end
+
+    play_again = ""
+      until play_again.downcase == "n" || play_again.downcase == "no"
+    puts "Would you like to play again?"
+    play_again = gets.strip
+    play_again.downcase == "y" || play_again.downcase == "yes"
+      game = Game.new
+      until game.won?
+        game.play
+      end
+    end
   end 
   
   def play 
