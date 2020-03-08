f43ecd8e940eddbf9e25e44b8c96704bc99921e5
Megan Horsfield <meganthadev@gmail.com>
2020-03-08 01:54:47 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -42,10 +42,28 @@ class Game
     until game.won?
      game.play
       end
+    puts "\nWelcome to Command Line Tic Tac Toe!"
+  puts "\nWhat kind of game will you like to play? Please choose player mode:
+  0 - Computer VS Computer
+  1 - You VS Computer
+  2 - You VS Your friend"
 
-    play_again = ""
-      until play_again.downcase == "n" || play_again.downcase == "no"
-    puts "Would you like to play again?"
+  player_mode = gets.strip
+
+  if player_mode == "1"
+    puts "Do you want to go first? [y/ n]"
+    if gets.strip == "y"
+      Game.new(Players::Human.new("X"), Players::Computer.new("O"), Board.new).play
+    else   Game.new(Players::Computer.new("X"), Players::Human.new("O"), Board.new).play
+    end
+
+  elsif player_mode == "0"
+    Game.new(Players::Computer.new("X"), Players::Computer.new("O"), Board.new).play
+
+  elsif player_mode == "2"
+    Game.new(Players::Human.new("X"), Players::Human.new("O"), Board.new).play
+  end
+  puts "Would like to play again? [y/ n]"
     play_again = gets.strip
     play_again.downcase == "y" || play_again.downcase == "yes"
       game = Game.new
@@ -53,7 +71,7 @@ class Game
         game.play
       end
     end
-  end 
+  
   
   def play 
     while !over?
